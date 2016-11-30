45a7429eea2490cfb1663c3775ac449ac2949d6c
wokar <wokar@users.noreply.github.com>
2016-11-30 20:00:15 +0100

    version bump to 0.3.0 - basic support for hdcp protocol of pre 2012 LG TVs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='pylgnetcast',
-            version='0.2.0',
+            version='0.3.0',
             description='Client for the LG Smart TV running NetCast 3 or 4.',
             url='https://github.com/wokar/pylgnetcast',
             license='MIT',
